edf222d782b68c3dd8d20768f2fffc682160dd7f
Mallvin2000 <mallvin2000@gmail.com>
2020-09-03 17:04:19 +0800

    edit profile page working except for email

---
+++ js/profile.js
@@ -0,0 +1,124 @@
+function getUserDataFromBackend() {
+    //event.preventDefault(); dont need to as no form is being submitted
+
+    var settings = {
+        "url": "http://localhost:3000/get/user/data",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer " + localStorage["token"]
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        //console.log(response);
+        $('#username').attr("placeholder", response[0].username);
+    });
+}
+
+
+function verifyPassword() {
+    var password = $("#password").val()
+    var confirmPassword = $("#confirm-password").val()
+    console.log(password);
+    if (password == confirmPassword) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+
+function sendDataToBackend(event) {
+    event.preventDefault();
+
+
+
+    if (verifyPassword) {
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        if (username.length == 0) {
+            username = $('#username').attr("placeholder");
+        }
+
+        if (password.length == 0 || password == "") {
+            var settings = {
+                "url": "http://localhost:3000/get/user/data",
+                "method": "GET",
+                "timeout": 0,
+                "headers": {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": "Bearer " + localStorage["token"]
+                }
+            };
+
+            $.ajax(settings).done(function (response) {
+                //console.log(response);
+                password = response[0].password;
+                var settings = {
+                    "url": "http://localhost:3000/update/user",
+                    "method": "PUT",
+                    "timeout": 0,
+                    "headers": {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": "Bearer " + localStorage["token"]
+                    },
+                    "data": {
+                        "username": username,
+                        "password": password
+                    }
+                };
+
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                    alert("Updated sucessfully")
+                })
+                    .fail((response) => {
+                        alert("ERROR");
+                    });
+            });
+
+        } else {
+            var settings = {
+                "url": "http://localhost:3000/update/user",
+                "method": "PUT",
+                "timeout": 0,
+                "headers": {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": "Bearer " + localStorage["token"]
+                },
+                "data": {
+                    "username": username,
+                    "password": password
+                }
+            };
+
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+                alert("Updated sucessfully")
+            })
+                .fail((response) => {
+                    alert("ERROR");
+                });
+        }
+
+    }
+
+
+}
+
+
+function registerUpdateForm() {
+    $('#edit-form').submit(sendDataToBackend);
+}
+
+
+
+
+$(document).ready(function () {//run when document is populated
+    getUserDataFromBackend();
+    registerUpdateForm();
+
+});
